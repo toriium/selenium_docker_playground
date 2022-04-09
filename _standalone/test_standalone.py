3a94b5c1607216950579d6bf56f224682db117b5
Jorge Vasconcelos <jorge.r.p.v@hotmail.com>
2022-04-09 14:57:23 -0300

    feat: container_selenium

--- _standalone/test_standalone.py
+++ _standalone/test_standalone.py
@@ -5,6 +5,9 @@ options.add_argument('--ignore-ssl-errors=yes')
 options.add_argument('--ignore-certificate-errors')
 driver = webdriver.Remote(command_executor='http://localhost:4444/wd/hub', options=options)
 
+# if running with other service on docker-compose
+# driver = webdriver.Remote(command_executor='http://container_selenium:4444/wd/hub', options=options)
+
 driver.get('https://hub.docker.com/u/selenium')
 
 driver.quit()
